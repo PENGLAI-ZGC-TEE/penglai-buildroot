a9a9e4500aa76481e7657660bba342c75e639b46
abcsml <a338383154@163.com>
2026-06-01 20:04:39 +0800

    Remove unused hello and poweroff packages

--- br2-external/board/nanhuv3a/nemu/post_build.sh
+++ br2-external/board/nanhuv3a/nemu/post_build.sh
@@ -3,10 +3,3 @@ set -eu
 
 chmod 0755 "${TARGET_DIR}/etc/init.d/rcS"
 chmod 0755 "${TARGET_DIR}/etc/init.d/S50dsid"
-
-mkdir -p "${TARGET_DIR}/root/scripts"
-
-if [ -d "${BR2_EXTERNAL_ZGC_TEE_PATH}/../riscv-rootfs/rootfsimg/scripts" ]; then
-	cp -a "${BR2_EXTERNAL_ZGC_TEE_PATH}/../riscv-rootfs/rootfsimg/scripts/." "${TARGET_DIR}/root/scripts/"
-	find "${TARGET_DIR}/root/scripts" -type f -name "*.sh" -exec chmod 0755 {} +
-fi
